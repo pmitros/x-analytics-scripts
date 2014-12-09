1ebf43de1ae708ab931d10843310e33055eadd99
Piotr Mitros <pmitros@mitx.mit.edu>
2014-12-09 12:40:21 -0500

    Added cluster and resources to setup.py (under unique names)

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='xanalytics', 
-    packages = ['xanalytics'],
+    packages = ['xanalytics', 'xcluster', 'xresources'],
     long_description = "Random analytics scripts over edX data. You probably don't want this."
 )
