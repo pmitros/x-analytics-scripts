f94c2aeefeaf93ab07c1644cc406407ac5284ff7
Piotr Mitros <pmitros@mitx.mit.edu>
2014-12-09 11:12:33 -0500

    Moving resource_grabber to use settings files

--- resources/resource_grabber.py
+++ resources/resource_grabber.py
@@ -6,6 +6,9 @@ WITH SPLIT MONGO*. It is likely to explode after this change.
 Run as: 
   python resource_grabber.py username password problem mongodb://database.24.mongolayer.com:27107/my-clone-database") my-clone-database
 
+Or:
+  python problem
+
 This script is untested in the current incarnation! However, the
 only change since the tested incarnation is that:
 
@@ -18,21 +21,39 @@ Used to be hardcoded to edX production servers.
 import argparse
 import json
 import sys
+from xanalytics import settings
+
 from pymongo import MongoClient
 
-parser = argparse.ArgumentParser(description = __doc__)
-parser.add_argument("username")
-parser.add_argument("password")
-parser.add_argument("resource_type")
-parser.add_argument("url")
-parser.add_argument("database")
-parser.parse_args()
+print settings.settings._settings
+print ("mongo-replica-url" in settings.settings)
+print len(sys.argv)
+print settings.settings['mongo-replica-url']
 
-client = MongoClient(sys.argv[4])
-db = client[sys.argv[5]]
-resource_type = sys.argv[3]
+if ("mongo-replica-url" in settings.settings) and len(sys.argv) < 3:
+    url = settings.settings["mongo-replica-url"]
+    username = settings.settings["mongo-replica-username"]
+    password = settings.settings["mongo-replica-password"]
+    db = settings.settings["mongo-replica-database"]
+    resource_type = sys.argv[1]
+else:
+    parser = argparse.ArgumentParser(description = __doc__)
+    parser.add_argument("username")
+    parser.add_argument("password")
+    parser.add_argument("resource_type")
+    parser.add_argument("url")
+    parser.add_argument("database")
+    parser.parse_args()
+    url = parser.url
+    username = parser.username
+    password = parser.password
+    db = parser.database
+    resource_type = parser.resource_type
+
+client = MongoClient(url)
+db = client[db]
 collection = db['modulestore']
-db.authenticate(sys.argv[1], sys.argv[2])
+db.authenticate(username, password)
 c = collection.find({"_id.category":resource_type})
 for item in c:
     id = item['_id']
